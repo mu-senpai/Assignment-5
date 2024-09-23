126cffa8fa0544c671c69de4c8516fe9dee983b1
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-09-23 20:40:47 +0530

    Backdrop blur for navbar and toggle buttons has modified.

--- js/navbarBgChange.js
+++ js/navbarBgChange.js
@@ -3,10 +3,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
-            navbar.classList.add('bg-transparent', 'backdrop-blur-md', 'scrolled');
+            navbar.classList.add('bg-transparent', 'backdrop-blur-lg', 'scrolled');
             navbar.classList.remove('bg-[#F9F7F3]');
         } else {
-            navbar.classList.remove('bg-transparent', 'backdrop-blur-md', 'scrolled');
+            navbar.classList.remove('bg-transparent', 'backdrop-blur-lg', 'scrolled');
             navbar.classList.add('bg-[#F9F7F3]');
         }
     })
