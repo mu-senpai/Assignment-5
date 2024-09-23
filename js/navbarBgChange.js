4f642348c3dd7e7e51b0b65a870c111824361c85
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-09-23 20:36:42 +0530

    Functionality to change the background of navbar after starting to scroll has added.

---
+++ js/navbarBgChange.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('header div');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-transparent', 'backdrop-blur-md', 'scrolled');
+            navbar.classList.remove('bg-[#F9F7F3]');
+        } else {
+            navbar.classList.remove('bg-transparent', 'backdrop-blur-md', 'scrolled');
+            navbar.classList.add('bg-[#F9F7F3]');
+        }
+    })
+})
