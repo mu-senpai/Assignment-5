01c67a0d2c310073dca74b03215459ce69c26859
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-09-24 11:07:26 +0530

    Navbar backgroud has modified again while scrolling.

--- js/navbarBgChange.js
+++ js/navbarBgChange.js
@@ -1,14 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('header div');
+    const navbarContainer = document.querySelector('header div');
     const header = document.querySelector('header');
+    const navbar = this.document.getElementById('navbar');
+    const toggleButtonsContainer = document.getElementById('toggle-btns');
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
             header.classList.add('bg-transparent', 'backdrop-blur-lg');
-            navbar.classList.remove('bg-[#F9F7F3]');
+            navbarContainer.classList.remove('bg-[#F9F7F3]');
+            navbar.classList.remove('bg-transparent');
+            toggleButtonsContainer.classList.remove('bg-transparent', 'backdrop-blur-lg');
         } else {
             header.classList.remove('bg-transparent', 'backdrop-blur-lg');
-            navbar.classList.add('bg-[#F9F7F3]');
+            navbarContainer.classList.add('bg-[#F9F7F3]');
+            navbar.classList.add('bg-transparent');
+            toggleButtonsContainer.classList.add('bg-transparent', 'backdrop-blur-lg');
         }
     })
 })
